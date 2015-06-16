756c887d0c51170ff37e78bb740396666d4c1c0c
Nikolay Cholakov <4okito@gmail.com>
2015-06-16 03:25:34 +0300

    ch0k1.org: MailTo fix

--- ch0k1.org/mail/contact_me.php
+++ ch0k1.org/mail/contact_me.php
@@ -1,14 +1,13 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
+if(empty($_POST['name']) ||
+   empty($_POST['email']) ||
+   empty($_POST['phone']) ||
+   empty($_POST['message']) ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+	echo "Bad request!!!";
 	return false;
-   }
+}
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
@@ -16,11 +15,28 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'chkorg1@ch0k1.org'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@ch0k1.org\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+
+// Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'chkorg1@ch0k1.org';
+
+// This is the mail subject
+$email_subject = 'Website Contact Form: ' .$name;
+
+// This is the mail body
+$email_body = 'You have received a new message from your website contact form.' ."\n\n";
+$email_body .= 'Here are the details:' ."\n\n";
+$email_body .= 'Name: '. $name ."\n\n";
+$email_body .= 'Email: '. $email_address ."\n\n";
+$email_body .= 'Phone: '. $phone ."\n\n";
+$email_body .= 'Message: '. "\n" . $message ."\n\n";
+
+$headers   = array();
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type: text/plain; charset=UTF-8";
+$headers[] = "From: noreply@ch0k1.org";
+$headers[] = "Reply-To: {$email_address}";
+$headers[] = "Subject: {$email_subject}";
+$headers[] = "X-Mailer: PHP/".phpversion();
+
 mail($to,$email_subject,$email_body,$headers);
-return true;			
 ?>
