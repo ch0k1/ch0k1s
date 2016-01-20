f7003f2d999d43472528cb14dbb06eb4b29da362
Nikolay Cholakov <nikolay.cholakov@tu-sofia.bg>
2016-01-21 00:31:45 +0200

    ch0k1.net: Reformat the code in all project files

--- ch0k1.com/js/script.js
+++ ch0k1.com/js/script.js
@@ -1,15 +1,14 @@
-
-$(document).ready(function(e) {
-	$('.with-hover-text, .regular-link').click(function(e){
+$(document).ready(function (e) {
+	$('.with-hover-text, .regular-link').click(function (e) {
 		e.stopPropagation();
 	});
-	
+
 	/***************
-	* = Hover text *
-	* Hover text for the last slide
-	***************/
+	 * = Hover text *
+	 * Hover text for the last slide
+	 ***************/
 	$('.with-hover-text').hover(
-		function(e) {
+		function (e) {
 			$(this).css('overflow', 'visible');
 			$(this).find('.hover-text')
 				.show()
@@ -24,7 +23,7 @@ $(document).ready(function(e) {
 					'linear'
 				);
 		},
-		function(e) {
+		function (e) {
 			var obj = $(this);
 			$(this).find('.hover-text')
 				.animate(
@@ -34,90 +33,89 @@ $(document).ready(function(e) {
 					},
 					'fast',
 					'linear',
-					function() {
+					function () {
 						$(this).hide();
-						$( obj ).css('overflow', 'hidden');
+						$(obj).css('overflow', 'hidden');
 					}
 				);
 		}
 	);
-	
+
 	var img_loaded = 0;
-	
+
 	/*************************
-	* = Controls active menu *
-	* Hover text for the last slide
-	*************************/
-	$('#slide-3 img').each(function(index, element) {
+	 * = Controls active menu *
+	 * Hover text for the last slide
+	 *************************/
+	$('#slide-3 img').each(function (index, element) {
 		var time = new Date().getTime();
 		var oldHref = $(this).attr('src');
-		var myImg = $('<img />').attr('src', oldHref + '?' + time );
-		
-		myImg.load(function(e) {
+		var myImg = $('<img />').attr('src', oldHref + '?' + time);
+
+		myImg.load(function (e) {
 			img_loaded += 1;
-			if ( img_loaded == $('#slide-3 img').length ) {
-				$(function() {
+			if (img_loaded == $('#slide-3 img').length) {
+				$(function () {
 					var pause = 10;
-					$(document).scroll(function(e) {
-						delay(function() {
-							
-							var tops = [];
-							
-							$('.story').each(function(index, element) {
-								tops.push( $(element).offset().top - 200 );
-							});
-				
-							var scroll_top = $(this).scrollTop();
-							
-							var lis = $('.nav > li');
-							
-							for ( var i=tops.length-1; i>=0; i-- ) {
-								if ( scroll_top >= tops[i] ) {
-									menu_focus( lis[i], i+1 );
-									break;
+					$(document).scroll(function (e) {
+						delay(function () {
+
+								var tops = [];
+
+								$('.story').each(function (index, element) {
+									tops.push($(element).offset().top - 200);
+								});
+
+								var scroll_top = $(this).scrollTop();
+
+								var lis = $('.nav > li');
+
+								for (var i = tops.length - 1; i >= 0; i--) {
+									if (scroll_top >= tops[i]) {
+										menu_focus(lis[i], i + 1);
+										break;
+									}
 								}
-							}
-						},
-						pause);
+							},
+							pause);
 					});
 					$(document).scroll();
 				});
 			}
 		});
 	});
-	
 });
 
 /******************
-* = Gallery width *
-******************/
-$(function() {
+ * = Gallery width *
+ ******************/
+$(function () {
 	var pause = 50; // will only process code within delay(function() { ... }) every 100ms.
-	$(window).resize(function() {
-		delay(function() {
+	$(window).resize(function () {
+		delay(function () {
 				var gallery_images = $('#slide-3 img');
-				
-				var images_per_row = gallery_images.length;
+				var gallery_row = $('#slide-3 .row');
+				//var images_per_row = gallery_images.length;
 				//if ( gallery_images.length % 2 == 0 ) {
 				//	images_per_row = gallery_images.length / 2;
 				//} else {
 				//	images_per_row = gallery_images.length / 2 + 1;
 				//}
-				
-				var gallery_width = $('#slide-3 img').width() * $('#slide-3 img').length;
+
+				var gallery_width = gallery_images.width() * gallery_images.length;
 				//gallery_width /= 2;
 				//if ( $('#slide-3 img').length % 2 != 0 ) {
 				//	gallery_width += $('#slide-3 img').width();
 				//}
-				
-				$('#slide-3 .row').css('width', gallery_width + 15);
-				
+
+				gallery_row.css('width', gallery_width + 15);
+
 				//var left_pos = $('#slide-3 .row').width() - $('body').width();
 				//left_pos /= -2;
 				//
 				//$('#slide-3 .row').css('left', left_pos);
-				$('#slide-3 .row').css('left', -500);
-			
+				gallery_row.css('left', -500);
+
 			},
 			pause
 		);
@@ -125,39 +123,39 @@ $(function() {
 	$(window).resize();
 });
 
-var delay = (function(){
+var delay = (function () {
 	var timer = 0;
-	return function(callback, ms){
-		clearTimeout (timer);
+	return function (callback, ms) {
+		clearTimeout(timer);
 		timer = setTimeout(callback, ms);
 	};
 })();
 
-function menu_focus( element, i ) {
-	if ( $(element).hasClass('active') ) {
-		if ( i == 6 ) {
-			if ( $('.navbar').hasClass('inv') == false )
+function menu_focus(element, i) {
+	if ($(element).hasClass('active')) {
+		if (i == 6) {
+			if ($('.navbar').hasClass('inv') == false)
 				return;
 		} else {
 			return;
 		}
 	}
-	
-	enable_arrows( i );
-		
-	if ( i == 1 || i == 6 )
+
+	enable_arrows(i);
+
+	if (i == 1 || i == 6)
 		$('.navbar').removeClass('inv');
 	else
 		$('.navbar').addClass('inv');
-	
+
 	$('.nav > li').removeClass('active');
 	$(element).addClass('active');
-	
+
 	var icon = $(element).find('.icon');
-	
+
 	var left_pos = icon.offset().left - $('.nav').offset().left;
 	var el_width = icon.width() + $(element).find('.text').width() + 10;
-	
+
 	$('.active-menu').stop(false, false).animate(
 		{
 			left: left_pos,
@@ -168,23 +166,23 @@ function menu_focus( element, i ) {
 	);
 }
 
-function enable_arrows( dataslide ) {
+function enable_arrows(dataslide) {
 	$('#arrows div').addClass('disabled');
-	if ( dataslide != 1 ) {
+	if (dataslide != 1) {
 		$('#arrow-up').removeClass('disabled');
 	}
-	if ( dataslide != 6 ) {
+	if (dataslide != 6) {
 		$('#arrow-down').removeClass('disabled');
 	}
-	if ( dataslide == 3 ) {
+	if (dataslide == 3) {
 		$('#arrow-left').removeClass('disabled');
 		$('#arrow-right').removeClass('disabled');
 	}
 }
 
 /*************
-* = Parallax *
-*************/
+ * = Parallax *
+ *************/
 jQuery(document).ready(function ($) {
 	//Cache some variables
 	var links = $('.nav').find('li');
@@ -192,17 +190,17 @@ jQuery(document).ready(function ($) {
 	button = $('.button');
 	mywindow = $(window);
 	htmlbody = $('html,body');
-	
+
 	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
 	//easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
 	function goToByScroll(dataslide) {
 		var offset_top = ( dataslide == 1 ) ? '0px' : $('.slide[data-slide="' + dataslide + '"]').offset().top;
-		
+
 		htmlbody.stop(false, false).animate({
 			scrollTop: offset_top
 		}, 1500, 'easeInOutQuart');
 	}
-	
+
 	//When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
 	links.click(function (e) {
 		e.preventDefault();
@@ -210,7 +208,7 @@ jQuery(document).ready(function ($) {
 		goToByScroll(dataslide);
 		$(".nav-collapse").collapse('hide');
 	});
-	
+
 	//When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
 	$('.navigation-slide').click(function (e) {
 		e.preventDefault();
@@ -221,43 +219,43 @@ jQuery(document).ready(function ($) {
 });
 
 /***************
-* = Menu hover *
-***************/
+ * = Menu hover *
+ ***************/
 jQuery(document).ready(function ($) {
 	//Cache some variables
 	var menu_item = $('.nav').find('li');
-	
+
 	menu_item.hover(
-		function(e) {
+		function (e) {
 			var icon = $(this).find('.icon');
-			
+
 			var left_pos = icon.offset().left - $('.nav').offset().left;
 			var el_width = icon.width() + $(this).find('.text').width() + 10;
-			
+
 			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
 				.css('left', left_pos)
 				.css('width', el_width)
-				.attr('id', 'special-active-menu-' + $(this).data('slide') );
-			
-			$('.active-menu').after( hover_bar );
+				.attr('id', 'special-active-menu-' + $(this).data('slide'));
+
+			$('.active-menu').after(hover_bar);
 		},
-		function(e) {
+		function (e) {
 			$('.special-active-menu').remove();
 		}
 	);
 });
 
 /******************
-* = Gallery hover *
-******************/
+ * = Gallery hover *
+ ******************/
 jQuery(document).ready(function ($) {
 	//Cache some variables
 	var images = $('#slide-3 a');
-	
+
 	images.hover(
-		function(e) {
+		function (e) {
 			var asta = $(this).find('img');
-			$('#slide-3 img').not( asta ).stop(false, false).animate(
+			$('#slide-3 img').not(asta).stop(false, false).animate(
 				{
 					opacity: .5
 				},
@@ -265,12 +263,12 @@ jQuery(document).ready(function ($) {
 				'linear'
 			);
 			var zoom = $('<div class="zoom"></div>');
-			if ( $(this).hasClass('video') ) {
+			if ($(this).hasClass('video')) {
 				zoom.addClass('video');
 			}
 			$(this).prepend(zoom);
 		},
-		function(e) {
+		function (e) {
 			$('#slide-3 img').stop(false, false).animate(
 				{
 					opacity: 1
@@ -284,18 +282,18 @@ jQuery(document).ready(function ($) {
 });
 
 /******************
-* = Arrows click  *
-******************/
+ * = Arrows click  *
+ ******************/
 jQuery(document).ready(function ($) {
 	//Cache some variables
 	var arrows = $('#arrows div');
-	
-	arrows.click(function(e) {
+
+	arrows.click(function (e) {
 		e.preventDefault();
-		
-		if ( $(this).hasClass('disabled') )
+
+		if ($(this).hasClass('disabled'))
 			return;
-		
+
 		var slide = null;
 		var datasheet = $('.nav > li.active').data('slide');
 		var offset_top = false;
@@ -303,46 +301,46 @@ jQuery(document).ready(function ($) {
 
 		$("#arrow-left").removeClass("disabled");
 		$("#arrow-right").removeClass("disabled");
-		
-		switch( $(this).attr('id') ) {
+
+		switch ($(this).attr('id')) {
 			case 'arrow-up':
-				offset_top = ( datasheet - 1 == 1 ) ? '0px' : $('.slide[data-slide="' + (datasheet-1) + '"]').offset().top;
+				offset_top = ( datasheet - 1 == 1 ) ? '0px' : $('.slide[data-slide="' + (datasheet - 1) + '"]').offset().top;
 				break;
 			case 'arrow-down':
-				offset_top = $('.slide[data-slide="' + (datasheet+1) + '"]').offset().top;
+				offset_top = $('.slide[data-slide="' + (datasheet + 1) + '"]').offset().top;
 				break;
 			case 'arrow-left':
 				offset_left = $('#slide-3 .row').offset().left + $("#slide-3 img").width();
-				if ( offset_left >= $("#slide-3 .row").width() ) {
+				if (offset_left >= $("#slide-3 .row").width()) {
 					offset_left = -15;
 					$("#arrow-right").removeClass("disabled");
 				}
 
-				if ( offset_left >= -15) {
+				if (offset_left >= -15) {
 					$("#arrow-left").addClass("disabled");
 				}
 				break;
 			case 'arrow-right':
 				offset_left = $('#slide-3 .row').offset().left - $("#slide-3 img").width();
-				if ( Math.abs(offset_left) >= $("#slide-3 img").width() * ($("#slide-3 img").length - 1) ) {
-					offset_left = -($("#slide-3 img").width() * ($("#slide-3 img").length -1));
+				if (Math.abs(offset_left) >= $("#slide-3 img").width() * ($("#slide-3 img").length - 1)) {
+					offset_left = -($("#slide-3 img").width() * ($("#slide-3 img").length - 1));
 					$("#arrow-left").addClass("disabled");
 				}
 
-				if (Math.abs(offset_left - $("#slide-3 img").width()) >= $("#slide-3 img").width() * ($("#slide-3 img").length - 1) ) {
+				if (Math.abs(offset_left - $("#slide-3 img").width()) >= $("#slide-3 img").width() * ($("#slide-3 img").length - 1)) {
 					$("#arrow-right").addClass("disabled");
 				}
 				break;
 		}
-		
-		if ( offset_top != false ) {
+
+		if (offset_top != false) {
 			htmlbody.stop(false, false).animate({
 				scrollTop: offset_top
 			}, 1500, 'easeInOutQuart');
 		}
-		
-		if ( offset_left != false ) {
-			if ( $('#slide-3 .row').width() != $('body').width() ) {
+
+		if (offset_left != false) {
+			if ($('#slide-3 .row').width() != $('body').width()) {
 				$('#slide-3 .row').stop(false, false).animate({
 					left: offset_left
 				}, 1500, 'easeInOutQuart');
